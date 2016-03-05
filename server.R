35e7c0d1bbc563b9371cbbe96c937a3e59f45aec
Esther Chang <esther3587@users.noreply.github.com>
2016-03-05 11:18:20 -0800

    Update server.R
    Orange Tree predictor

--- server.R
+++ server.R
@@ -1,10 +1,23 @@
 library(shiny)
+library(datasets)
+data(Orange)
 
-area <- function(diameter) (diameter/2)^2*3.14
+Tyear <- function(Tcir) {
+    fit<- lm(age~circumference,data=Orange)
+    mod <- predict(fit,newdata=data.frame(circumference=Tcir),interval="predict")
+    mod[1]
+}
+
 shinyServer(
     function(input, output) {
-    output$inputValue <- renderPrint({input$diameter})
-    output$prediction <- renderPrint({area(input$diameter)})
+    output$inputValue <- renderPrint({input$Tcir})
+    output$prediction <- renderPrint({Tyear(input$Tcir)})
+    output$scatterPlot <- renderPlot({
+        plot(Orange$circumference,Orange$age, xlab = "Tree Circumference (mm)",ylab="Tree Age (year)",col = 'blue',pch=16,cex=1.5)
+        abline(lm(age~circumference,data=Orange),lwd=2)
+        circum <- input$Tcir
+        lines(c(circum,circum),c(0,2000),col="red",lwd="5")
+    })
     
     }
 )
